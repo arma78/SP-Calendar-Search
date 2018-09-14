1b7a92e8029280073623c141d947400bfb61c717
arma78 <37850817+arma78@users.noreply.github.com>
2018-09-14 17:41:09 +0200

    Add files via upload

---
+++ LoadCSVintoSharePointList.ps1
@@ -0,0 +1,51 @@
+﻿#-------------------------------------------------------------------- 
+# Name: Load CSV into SharePoint List 
+#--------------------------------------------------------------------
+
+
+
+# Setup the correct modules for SharePoint Manipulation 
+if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null ) 
+{ 
+   Add-PsSnapin Microsoft.SharePoint.PowerShell 
+} 
+$host.Runspace.ThreadOptions = "ReuseThread" 
+
+#Open SharePoint List 
+$SPServer=http://kndev-stage
+$SPAppList="/Lists/News%20Articles/" 
+$spWeb = Get-SPWeb $SPServer 
+$spData = $spWeb.GetList($SPAppList) 
+
+$InvFile="NameOfExcell.csv" 
+# Get Data from Excell CSV File 
+$FileExists = (Test-Path $InvFile -PathType Leaf) 
+if ($FileExists) { 
+   "Loading $InvFile for processing..." 
+   $tblData = Import-CSV $InvFile 
+} else { 
+   "$InvFile not found - stopping import!" 
+   exit 
+} 
+
+# Loop through Applications add each one to SharePoint 
+
+"Uploading data to SharePoint...." 
+
+foreach ($row in $tblData) 
+{ 
+   "Adding entry for "+$row."Application Name".ToString() 
+   $spItem = $spData.AddItem() 
+   $spItem["Title"] = $row."Title".ToString() 
+   $spItem["News Articles Titles"] = $row."NEWS_ITM_HDLN_TXT".ToString()
+   $spItem["News Article"] = $row."Body".ToString() 
+  # $spItem["Article Publishing Date"] = $row."Matching_one_in_CSV".ToString() 
+  # $spItem["Publishing Checker"] = $row."Matching_one_in_CSV"
+  # $spItem["Mail Sent"] = $row."Matching_one_in_CSV"
+   $spItem.Update() 
+} 
+
+"---------------" 
+"Upload Complete" 
+
+$spWeb.Dispose()
---
+++ ProvisionDisplayTemplate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using System.Collections.Generic;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.Linq;
+using System.Security.Policy;
+
+public class ProvisionDisplayTemplate
+{
+    public ProvisionDisplayTemplate()
+	{
+	}
+    public string[] folderUrls = { "_catalogs/masterpage/Display Templates/Search" };
+
+    public void DisplayTemplate()
+    {
+    SPSite site = properties.Feature.Parent as SPSite; 
+            if (site != null) 
+            { 
+                SPWeb rootWeb = site.RootWeb;
+                SPList gallery = site.GetCatalog(SPListTemplateType.MasterPageCatalog);
+                if (gallery != null) 
+                { 
+                    SPListItemCollection folders = gallery.Folders; 
+                    string featureId = properties.Feature.Definition.Id.ToString();
+                    foreach (string folderUrl in folderUrls) 
+                    { 
+                        SPFolder folder = GetFolderByUrl(folders, folderUrl); 
+                        if (folder != null) 
+                        { 
+                            PublishFiles(folder, featureId); 
+                        } 
+                    } 
+                } 
+            }
+      }
+    private static SPFolder GetFolderByUrl(SPListItemCollection folders, string folderUrl)
+    {
+        if (folders == null)
+        {
+            throw new ArgumentNullException("folders");
+        }
+        if (String.IsNullOrEmpty(folderUrl))
+        {
+            throw new ArgumentNullException("folderUrl");
+        }
+        SPFolder folder = null;
+
+        SPListItem item = (from SPListItem i
+                           in folders
+                           where i.Url.Equals(folderUrl, StringComparison.InvariantCultureIgnoreCase)
+                           select i).FirstOrDefault();
+        if (item != null)
+        {
+            folder = item.Folder;
+        }
+        return folder;
+    }
+    private static void PublishFiles(SPFolder folder, string featureId)
+    {
+        if (folder == null)
+        {
+            throw new ArgumentNullException("folder");
+        }
+        if (String.IsNullOrEmpty(featureId))
+        {
+            throw new ArgumentNullException("featureId");
+        }
+        SPFileCollection files = folder.Files;
+        var drafts = from SPFile f
+                              in files
+                     where String.Equals(f.Properties["FeatureId"] as string, featureId, StringComparison.InvariantCultureIgnoreCase) &&
+                     f.Level == SPFileLevel.Draft
+                     select f;
+        foreach (SPFile f in drafts)
+        {
+            f.Publish("");
+            f.Update();
+        }
+
+    }
+
+}
